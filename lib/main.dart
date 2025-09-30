2d0855bd66743002649a1d868c2023d8ac6a96b0
Jay Chaudhary <82433052+JAY01-CYBER@users.noreply.github.com>
2025-09-30 10:24:20 +0530

    Create main.dart

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'screens/home_screen.dart';
+
+void main() {
+  runApp(const MyMusicApp());
+}
+
+class MyMusicApp extends StatelessWidget {
+  const MyMusicApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'MyMusic Pro',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const HomeScreen(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
